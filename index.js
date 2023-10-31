4e7b2e5f5bc417952ef8565ea4b11b2ffd3e3a6c
Jeffrey Davis <j.davis081248@gmail.com>
2023-10-31 17:49:40 -0500

    Creating and Inserting DOM Nodes Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const mainOne = document.getElementById("main");
+mainOne.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Jeffrey is the champion";
